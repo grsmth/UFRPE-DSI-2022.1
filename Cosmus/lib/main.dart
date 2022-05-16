653dd8b8806fcb56c1440f67b9001c8be88fb1c2
Felipe Santana <69775067+FelipeSantanaC@users.noreply.github.com>
2022-05-15 22:44:49 -0300

    app

--- Cosmus/lib/main.dart
+++ Cosmus/lib/main.dart
@@ -1,90 +1,49 @@
-import 'dart:convert';
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use, avoid_unnecessary_containers, unused_import
+
+import 'package:cosmus/telaGrafico.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main (){
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+    debugShowCheckedModeBanner: false
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({ Key? key }) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      backgroundColor: Color(0xFF525252),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("images/logo.jpg"),
+            ButtonTheme(
+              minWidth: 100.0,
+              height: 50.0,
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage(title: '',))
+                );
+                },
+                child: Text("Gráfico"),
+              ),
+            )
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<CustomerLocation> states = [];
-  Future <String> getJsonFromFirebase() async{
-    String url = 'https://exampledb-7ca46-default-rtdb.firebaseio.com/data.json';
-    http.Response response = await http.get(Uri.parse(url));
-    return response.body;
-  }
-  Future loadSalesData() async{
-    final String jsonString = await getJsonFromFirebase();
-    final dynamic jsonResponse = json.decode(jsonString);
-    for(Map<String, dynamic> i in jsonResponse){
-      states.add(CustomerLocation.fromjson(i));
-    }
-
-  }
-  @override
-  void initState(){
-    loadSalesData();
-    super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold (body: FutureBuilder(future: getJsonFromFirebase(), builder:(context, snapshot){
-      if (snapshot.hasData){
-        return (SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
-          series: <ChartSeries>[
-          LineSeries<CustomerLocation,String>(
-          dataSource: states,
-          xValueMapper: (CustomerLocation details, _) => details.state,
-          yValueMapper: (CustomerLocation details, _) => details.customers)
-        ]));
-    } else{
-        return Center(child: CircularProgressIndicator());
-      }
-    },
-      )));
-  }
-}
-class CustomerLocation{
-  CustomerLocation(this.state, this.customers);
-  final String state;
-  final int customers;
-  factory CustomerLocation.fromjson(Map<String, dynamic> parsedJson){
-    return CustomerLocation(parsedJson['state'].toString(), parsedJson['customers']);
   }
 }
